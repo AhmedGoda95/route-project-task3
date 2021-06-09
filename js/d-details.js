6675a96ba7b457d01ac0ecd85f08e5a0ead89b13
MohammedAtef74 <muhammed.3atf.95@gmail.com>
2021-06-09 23:42:04 +0200

    add d-details.js add alldoctors.js

---
+++ js/d-details.js
@@ -0,0 +1,8 @@
+let dimage = document.getElementById("doctor-photo");
+let dName =document.getElementById("d-name");
+let dDepartment =document.getElementById("department");
+
+doctor ={name:"محمد احمد",image:"", depart:" دكتور عظام"};
+
+  dName.innerHTML=doctor.name;
+  dDepartment.innerHTML=doctor.depart
