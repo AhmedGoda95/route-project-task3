f08959dcfb1332107d5a2fe7c832b891d57a8b70
Ahmed <abdallahabdo4444@gmail.com>
2021-06-10 13:29:23 +0200

    update doctors

--- js/d-details.js
+++
@@ -1,8 +0,0 @@
-let dimage = document.getElementById("doctor-photo");
-let dName =document.getElementById("d-name");
-let dDepartment =document.getElementById("department");
-
-doctor ={name:"محمد احمد",image:"", depart:" دكتور عظام"};
-
-  dName.innerHTML=doctor.name;
-  dDepartment.innerHTML=doctor.depart
